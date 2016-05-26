972facadd057fcc196f9a7edee0e272b7501242c
Luka Popijac <luka.popijac@gmail.com>
2016-05-25 23:48:44 -0700

    remove 0.0.0.0 from server.js

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static(publicDir));
 
 app.use(require('./search/app'));
 
-app.listen(port, '0.0.0.0', function() {
+app.listen(port, function() {
 	console.log('server started at port', port);
 });
