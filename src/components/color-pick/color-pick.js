4d46458baba424a346d236c71e53e2cbb3b42f3e
Luka Popijac <luka.popijac@gmail.com>
2019-06-09 16:04:21 -0700

    some refactoring of css, making responsive ui, round 2

--- src/components/color-pick/color-pick.js
+++ src/components/color-pick/color-pick.js
@@ -6,21 +6,6 @@ export class ColorPick extends HTMLElement {
 		super();
 		this.attachShadow({mode: 'open'});
 		this.shadowRoot.appendChild(template.content.cloneNode(true));
-		this._el = this.shadowRoot.querySelector('div');
-	}
-
-	static get observedAttributes() {
-		return ['val', 'selected'];
-	}
-
-	attributeChangedCallback(attrName, oldVal, newVal) {
-		if(attrName == 'val') {
-			this._el.setAttribute('val', newVal);
-		}
-		if(attrName == 'selected') {
-			if(newVal != null) this._el.setAttribute('selected', true);
-			else this._el.removeAttribute('selected');
-		}
 	}
 }
 
